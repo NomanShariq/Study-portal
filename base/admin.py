6c05c5c9d38ece91d4365a11c109af570a6e1c3e
Syed Noman Shariq <nshariq562@gmail.com>
2023-11-21 03:41:29 +0500

    created more tables and may more things added

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from base.models import Message, Room, Topic
+
 # Register your models here.
+
+admin.site.register(Room)
+
+admin.site.register(Topic)
+
+admin.site.register(Message)
+
--- base/views.py
+++ base/views.py
@@ -1,23 +1,21 @@
 from django.shortcuts import render
 
+from base.models import Room
+
 # Create your views here.
 
-rooms = [
-    {'id' : 1 , 'name' : 'Lets learn Python!!'},
-    {'id' : 2 , 'name' : 'Web dvelopers'},
-    {'id' : 3 , 'name' : 'Logo designers needed'}
-]
+# rooms = [
+#     {'id' : 1 , 'name' : 'Lets learn Python!!'},
+#     {'id' : 2 , 'name' : 'Web dvelopers'},
+#     {'id' : 3 , 'name' : 'Logo designers needed'}
+# ]
 
 def homepage(request):
+    rooms = Room.objects.all()
     context = {'rooms' : rooms}
     return render(request, 'base/home.html', context)
 
 def room(request,pk): 
-    room = None
-    
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
-            
+    room = Room.objects.get(id=pk)        
     context = {'rooms':room}
     return render(request,'base/room.html',context)
