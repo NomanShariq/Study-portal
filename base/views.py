bb48d84b9be1eca7e1229ecf7b06c7f6a5b763a8
Syed Noman Shariq <nshariq562@gmail.com>
2023-12-07 03:36:01 +0500

    create form modified and theme integreted

--- base/views.py
+++ base/views.py
@@ -108,7 +108,9 @@ def createRoom(request):
     if request.method == 'POST':
         form = Roomform(request.POST)
         if form.is_valid():
-            form.save()
+            room = form.save(commit=False)
+            room.host = request.user
+            room.save()
             return redirect('home')
     
     context = {'form': form}
