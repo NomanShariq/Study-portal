4d3695362da988302cd7c3acf08e31abdb9a6f27
Syed Noman Shariq <nshariq562@gmail.com>
2023-12-21 02:56:06 +0500

    added api work

--- base/api/views.py
+++ base/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from base.models import Room
+from .serializers import RoomSerializer
 
 @api_view(['GET'])
 def getRoutes(request):
@@ -14,6 +15,12 @@ def getRoutes(request):
 
 @api_view(['GET'])
 def getRooms(request):
-    rooms = Room.object.all()
-    
-    return Response(rooms)
+    rooms = Room.objects.all()
+    serializer = RoomSerializer(rooms, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getRoom(request,pk):
+    room = Room.objects.get(id=pk)
+    serializer = RoomSerializer(room, many=False)
+    return Response(serializer.data)
